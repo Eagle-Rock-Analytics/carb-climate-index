b902462b337c47c4e91361b749fb778371ed79e7
beth mcclenny <beth.e.mcclenny@gmail.com>
2024-01-26 14:39:18 -0800

    s3 up/download functionality for metadata files

--- scripts/utils/write_metadata.py
+++ scripts/utils/write_metadata.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from functools import wraps
 import os
-
+import boto3
+s3_client = boto3.client('s3')
 metadata_path = os.path.expanduser('~/metadata/')
 
 def make_metadata_files(df):
@@ -14,9 +15,10 @@ def make_metadata_files(df):
     for var in varlist:    
         vardat = df.loc[df['Variable'] == var]
         meta_dict = vardat.set_index('Variable').to_dict()
-        f = open(
-            f"{metadata_path}/{var}_metadata.txt", "w"
-        )
+        file_name = f"{var}_metadata.txt"
+        metadata_path = "3_fair_data/metadata/1_pull/"
+        obj_name = f"{metadata_path}{file_name}"
+        f = open(file_name, "w")
         f.write(
             "========"
             + " Metadata document "
@@ -43,14 +45,17 @@ def make_metadata_files(df):
             list(meta_dict.values())
         ):
             f.write(f"{key}: {val[var]}")
-            f.write("\n")
-        
+            f.write("\n")        
         f.write(
             "*** As of the making of this document ("+ datestr+"), "
             +"this work is still in development and "
             +"may not represent the final data product for ingestion "
             +"into the California Climate Risk and Adaptation Index. ***")
         f.close()
+        s3_client.upload_file(
+            file_name, 'ca-climate-index', obj_name)
+        print(f"Uploaded metadata file to {obj_name} in S3 bucket")
+        os.remove(file_name)
 
 def append_metadata(func):
     '''
@@ -60,14 +65,19 @@ def append_metadata(func):
     Note: varname is a required keyword argument, and must be supplied to the function! 
     '''
     def metadata_generator(*args, **kwargs):
+        metadata_path = "3_fair_data/metadata/1_pull/"
         # Call the function
         result = func(*args, **kwargs)
-
         # Write the function parameters to file
         now = datetime.now()
         datestr = now.strftime("%B %d, %Y")
-        var = kwargs['varname']
-        f = open(f"{metadata_path}/{var}_metadata.txt", "a")
+        var = kwargs['varname']       
+        # Download the metadata file from S3
+        file_name = f"{var}_metadata.txt"
+        obj_name = f"{metadata_path}{file_name}"
+        s3_client.download_file(
+            'ca-climate-index', obj_name, file_name)
+        f = open(file_name, "a")
         f.write("\n")
         f.write("\n")
         f.write("======== Function(s) applied to " + var + " ========")
@@ -89,6 +99,11 @@ def append_metadata(func):
         f.write("\n")
         f.write("\n")
         f.close()
+        # Upload the file to S3
+        s3_client.upload_file(
+            file_name, 'ca-climate-index', obj_name)
+        print(f"Updated metadata file in {obj_name} in S3 bucket")
+        os.remove(file_name)
         return result
     return metadata_generator
 
