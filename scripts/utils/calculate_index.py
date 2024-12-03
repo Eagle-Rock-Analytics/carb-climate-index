6b18a8e0326b81b353ce81b20a69eb4737f8ae8f
Jesse Espinoza <jesse@eaglerockanalytics.com>
2024-12-03 13:51:17 -0700

    geodatabase metadata

--- scripts/utils/calculate_index.py
+++ scripts/utils/calculate_index.py
@@ -345,7 +345,7 @@ def weight_domains(df, society, built, natural):
     natural: int
         Weighting modifier for natural domain
     '''
-    governance_col = 'governance_domain_index'
+    governance_col = 'governance_domain_score'
     society_adjusted_col = 'society_economy_tract_adjusted'
     built_adjusted_col = 'built_tract_adjusted'
     natural_adjusted_col = 'natural_systems_tract_adjusted' 
@@ -390,7 +390,7 @@ def calculate_equal_weighted_index(df):
     df: DataFrame
         Input dataframe  
     '''
-    governance_col = 'governance_domain_index'
+    governance_col = 'governance_domain_score'
     society_adjusted_col = 'society_economy_tract_adjusted'
     built_adjusted_col = 'built_tract_adjusted'
     natural_adjusted_col = 'natural_systems_tract_adjusted' 
@@ -401,10 +401,10 @@ def calculate_equal_weighted_index(df):
         (1 * (df[built_adjusted_col] * df[governance_col])) +
         (1 * (df[natural_adjusted_col] * df[governance_col]))
     )
-    df['calcrai_equal_weighted'] = weighting
+    df['calcrai_equal_weighted_numerator'] = weighting
 
     # divide by climate domain
-    df['calcrai_score'] = df['calcrai_equal_weighted'] / df['climate_risk']
+    df['calcrai_score'] = df['calcrai_equal_weighted_numerator'] / df['climate_risk']
 
     # testing for 0 values --> divide error
     df.loc[df['climate_risk'] == 0, 'calcrai_score'] = 0
